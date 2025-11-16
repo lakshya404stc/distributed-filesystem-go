d42ed4a8d6795ed060595902897ddc3d1ec199d9
lakshya404stc <lakshyag404stc@gmail.com>
2025-11-16 17:37:56 +0530

    update: read and write stream

--- store.go
+++ store.go
@@ -1,20 +1,44 @@
 package main
 
 import (
+	"bytes"
+	"crypto/md5"
+	"crypto/sha1"
+	"encoding/hex"
 	"io"
 	"log"
 	"os"
 	"path"
+	"strings"
 )
 
-func CASPathTransformFunc(key string) (string, error) {
- return key, nil
+func CASPathTransformFunc(key string) (PathKey, error) {
+	hash := sha1.Sum([]byte(key))
+	hashStr := hex.EncodeToString(hash[:])
+
+	blockSize := 5
+	sliceLen := len(hashStr) / blockSize
+	paths := make([]string, sliceLen)
+
+	for i := 0; i < sliceLen; i++ {
+		var from, to int = i * blockSize, (i * blockSize) + blockSize
+		path := hashStr[from:to]
+		paths[i] = path
+	}
+	fullPath := path.Join(paths...)
+
+	return PathKey{Pathname: fullPath, Original: hashStr}, nil
 }
 
-type PathTransformFunc func(string) (string, error)
+type PathKey struct {
+	Pathname string
+	Original string
+}
 
-func DefaultPathTransformFunc(key string) (string, error){
-	return key, nil
+type PathTransformFunc func(string) (PathKey, error)
+
+func DefaultPathTransformFunc(key string) (PathKey, error) {
+	return PathKey{key,key}, nil
 }
 
 type StoreOpts struct {
@@ -40,13 +64,18 @@ func (s *Store) writeStream(key string, r io.Reader) error {
 		return err
 	}
 
-	filename := "testFileName.txt"
-	pathAndFilename := path.Join(pathName, filename)
+	buf := new(bytes.Buffer)
+	io.Copy(buf, r)
+
+	filenameBuf := md5.Sum(buf.Bytes())
+	filenameString := hex.EncodeToString(filenameBuf[:])
+
+	pathAndFilename := path.Join(pathName, filenameString)
 	f, err := os.Create(pathAndFilename)
 	if err != nil {
 		return err
 	}
-	n, err := io.Copy(f, r) 
+	n, err := io.Copy(f, buf)
 	if err != nil {
 		return err
 	}
--- store_test.go
+++ store_test.go
@@ -2,12 +2,26 @@ package main
 
 import (
 	"bytes"
+	"fmt"
 	"testing"
 )
 
+
+func TestStorePathTransformFunc(t *testing.T) {
+	var key string = "testpathnumber1"
+	pathName, err := CASPathTransformFunc(key)
+	if err != nil {
+		t.Error(err) 
+	}
+	fmt.Println(pathName)
+	if pathName.Pathname != "2ad85/b1c05/d0930/01012/86512/80a47/010b7/48f1d" {
+		t.Error("[ERROR] Path name mismatch")
+	}
+}
+
 func TestStore(t *testing.T) {
 	var storeOpts StoreOpts = StoreOpts{
-		PathTransformFunc: DefaultPathTransformFunc,
+		PathTransformFunc: CASPathTransformFunc,
 	}
 
 	s := NewStore(storeOpts)
